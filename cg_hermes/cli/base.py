0e87e0509103e2b681c48f9d9904383670bf0fb0
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-12-10 14:07:12 +0100

    Adds version command with tests

--- cg_hermes/cli/base.py
+++ cg_hermes/cli/base.py
@@ -1,7 +1,9 @@
 import logging
 from enum import Enum
+from typing import Optional
 
 import coloredlogs
+import pkg_resources
 import typer
 
 from cg_hermes.cli import convert, export, validate
@@ -12,6 +14,9 @@ app.add_typer(convert.app, name="convert")
 app.add_typer(validate.app, name="validate")
 app.add_typer(export.app, name="export")
 
+__version__ = pkg_resources.get_distribution("cg_hermes").version
+LOG = logging.getLogger(__name__)
+
 
 class LogLevel(str, Enum):
     debug = "DEBUG"
@@ -19,9 +24,21 @@ class LogLevel(str, Enum):
     warning = "WARNING"
 
 
+def version_callback(value: bool):
+    if value:
+        typer.echo(f"hermes version: {__version__}")
+        raise typer.Exit()
+
+
 @app.callback()
-def main(loglevel: LogLevel = typer.Option(LogLevel.info, help="Set the log level")):
+def main(
+    loglevel: LogLevel = typer.Option(LogLevel.info, help="Set the log level"),
+    version: Optional[bool] = typer.Option(
+        None, "--version", callback=version_callback, is_eager=True
+    ),
+):
     """
     Manage users in the awesome CLI app.
     """
+    LOG.info("Running hermes")
     coloredlogs.install(level=loglevel.value)
---
+++ tests/cli/test_base_command.py
@@ -0,0 +1,27 @@
+"""Tests for the hermes base command"""
+import pkg_resources
+from typer.testing import CliRunner
+
+from cg_hermes.cli.base import app
+
+version = pkg_resources.get_distribution("cg_hermes").version
+
+
+def test_run_base(cli_runner: CliRunner):
+    # GIVEN a cli runner
+
+    # WHEN running the base command
+    result = cli_runner.invoke(app, [])
+
+    # THEN assert it exits without problem
+    assert result.exit_code == 0
+
+
+def test_run_version_command(cli_runner: CliRunner):
+    # GIVEN a cli runner
+
+    # WHEN running the base command with version flag
+    result = cli_runner.invoke(app, ["--version"])
+
+    # THEN assert the correct version is displayed
+    assert f"hermes version: {version}" in result.output
