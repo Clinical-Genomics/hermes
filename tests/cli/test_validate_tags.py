34ccc2b82ac84c27937372909e73c2268a120373
Mropat <mariaropat@gmail.com>
2021-03-23 10:11:40 +0100

    remove cg tags

--- tests/cli/test_validate_tags.py
+++ tests/cli/test_validate_tags.py
@@ -5,16 +5,6 @@ from typer.testing import CliRunner
 from cg_hermes.cli.validate import app
 
 
-def test_cli_validate_cg_tags(cli_runner: CliRunner):
-    # GIVEN a CLI runner
-
-    # WHEN testing to validate the common tags from the CLI
-    result = cli_runner.invoke(app, ["tags"])
-
-    # THEN assert that the validation was successful
-    assert result.exit_code == 0
-
-
 def test_cli_validate_mip_tags(cli_runner: CliRunner):
     # GIVEN a CLI runner
 
