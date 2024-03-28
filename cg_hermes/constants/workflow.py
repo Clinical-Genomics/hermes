3e3a7e95479967d8e5a808a370df12c5478d407e
Vadym <vadym.ivanchuk@scilifelab.se>
2024-03-28 11:13:42 +0100

    Sort alphabetically

--- cg_hermes/constants/workflow.py
+++ cg_hermes/constants/workflow.py
@@ -2,25 +2,25 @@ from enum import StrEnum
 
 
 class Workflow(StrEnum):
-    BALSAMIC: str = "balsamic"
+    BALSAMIC_PON: str = "balsamic-pon"
     BALSAMIC_QC: str = "balsamic-qc"
     BALSAMIC_UMI: str = "balsamic-umi"
-    BALSAMIC_PON: str = "balsamic-pon"
+    BALSAMIC: str = "balsamic"
     DEMULTIPLEX: str = "demultiplex"
     FASTQ: str = "fastq"
     FLUFFY: str = "fluffy"
     MICROSALT: str = "microsalt"
     MIP_DNA: str = "mip-dna"
     MIP_RNA: str = "mip-rna"
+    MUTANT: str = "mutant"
     RNAFUSION: str = "rnafusion"
     RSYNC: str = "rsync"
-    MUTANT: str = "mutant"
     SPRING: str = "spring"
     TAXPROFILER: str = "taxprofiler"
 
 
 class AnalysisType(StrEnum):
-    TUMOR_WGS: str = "tumor_wgs"
+    TUMOR_NORMAL_PANEL: str = "tumor_normal_panel"
     TUMOR_NORMAL_WGS: str = "tumor_normal_wgs"
     TUMOR_PANEL: str = "tumor_panel"
-    TUMOR_NORMAL_PANEL: str = "tumor_normal_panel"
+    TUMOR_WGS: str = "tumor_wgs"
