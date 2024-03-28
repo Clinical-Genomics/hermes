9f84927ecbbb9050f7dd3689ce34032afaf1f620
Vadym <vadym.ivanchuk@scilifelab.se>
2024-03-28 13:25:44 +0100

    Remove tags config

--- cg_hermes/validate.py
+++ cg_hermes/validate.py
@@ -3,7 +3,6 @@
 import logging
 from typing import FrozenSet
 
-from cg_hermes.config.tags import COMMON_TAG_CATEGORIES
 from cg_hermes.constants.workflow import CancerAnalysisType, Workflow
 from cg_hermes.deliverables import Deliverables
 from cg_hermes.models.tags import TagMap
@@ -20,24 +19,6 @@ def get_deliverables_obj(
         LOG.error(f"Please specify analysis type for {workflow}")
         raise SyntaxError
     return Deliverables(deliverables=deliverables, workflow=workflow, analysis_type=analysis_type)
-
-
-def validate_common_tags() -> bool:
-    """Validate the common tags"""
-    for category in COMMON_TAG_CATEGORIES:
-        tag_map: dict[str, dict[str, str]] = COMMON_TAG_CATEGORIES[category]
-        for tag_name, value in tag_map.items():
-            try:
-                assert isinstance(tag_map[tag_name], dict)
-            except AssertionError as err:
-                LOG.warning("Tag %s in %s is on the wrong format", tag_name, category.upper())
-                raise err
-            try:
-                assert "description" in value
-            except AssertionError as err:
-                LOG.warning("Tag %s in %s does not have a description", tag_name, category.upper())
-                raise err
-    return True
 
 
 def validate_tag_map(tag_map: dict[FrozenSet[str], dict]) -> bool:
