e0770580515cd4f4284784a4d513395d9bf439f6
Eva Caceres <eva.caceres@scilifelab.se>
2024-04-04 11:04:06 +0200

    add pytest for export tags for tomte

--- tests/cli/test_export_tags.py
+++ tests/cli/test_export_tags.py
@@ -1,8 +1,11 @@
 """Tests for the export cli command"""
 
+import pytest
+from _pytest.fixtures import FixtureRequest
 from typer.testing import CliRunner
 
 from cg_hermes.cli.export import app
+from cg_hermes.constants.workflow import Workflow
 
 
 def test_export_common_tags(cli_runner: CliRunner):
@@ -68,18 +71,6 @@ def test_export_fluffy_tags(cli_runner: CliRunner):
     assert "Fluffy tags" in result.output
 
 
-def test_export_rnafusion_tags(cli_runner: CliRunner):
-    # GIVEN a cli runner
-
-    # WHEN running the export tags command for workflow rnafusion
-    result = cli_runner.invoke(app, ["--workflow", "rnafusion"])
-
-    # THEN assert that the command exits without problems
-    assert result.exit_code == 0
-    # THEN assert that the rnafusion tags was exported
-    assert "Rnafusion tags" in result.output
-
-
 def test_export_microsalt_tags(cli_runner: CliRunner):
     # GIVEN a cli runner
 
@@ -104,13 +95,21 @@ def test_export_mutant_tags(cli_runner: CliRunner):
     assert "Mutant tags" in result.output
 
 
-def test_export_taxprofiler_tags(cli_runner: CliRunner):
-    # GIVEN a cli runner
-
-    # WHEN running the export tags command for Mutant
-    result = cli_runner.invoke(app, ["--workflow", "taxprofiler"])
+@pytest.mark.parametrize(
+    "workflow",
+    Workflow.get_nf_workflows(),
+)
+def test_export_nf_workflow_tags(
+    cli_runner: CliRunner,
+    workflow: Workflow,
+    request: FixtureRequest,
+):
+    """Test that tags are exported for nextflow workflows."""
+    # WHEN invoking the export tags command
+    result = cli_runner.invoke(app, ["--workflow", workflow])
 
     # THEN assert that the command exits without problems
     assert result.exit_code == 0
-    # THEN assert that the MicroSALT tags were exported
-    assert "Taxprofiler tags" in result.output
+
+    # THEN assert that tags were exported
+    assert f"{workflow.capitalize()} tags" in result.output
