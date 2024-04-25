e1d68d92d50951971ce3a4febb4a91947f004112
Eva Caceres <eva.caceres@scilifelab.se>
2024-04-25 11:52:34 +0200

    replace strings to constants

--- cg_hermes/config/nextflow.py
+++ cg_hermes/config/nextflow.py
@@ -1,15 +1,16 @@
 """Tags that are defined in nextflow deliverables mapped to tags used in CG
 
 The tag sets that exists in all files are set to mandatory. Tag sets that exists in < 4 deliverables are not
-mandatory by default. However the tags that are available to a particular analysis is mandatory for that analysis.
+mandatory by default. However, the tags that are available to a particular analysis is mandatory for that analysis.
 """
+from cg_hermes.constants.tags import UsageTags, NextflowTags
 
 NEXTFLOW_TAGS = {
     frozenset({"software-versions"}): {
         "is_mandatory": True,
         "bundle_id": True,
-        "tags": ["software-versions"],
-        "used_by": ["cg", "long-term-storage"],
+        "tags": [NextflowTags.SOFTWARE_VERSIONS],
+        "used_by": [UsageTags.CG, UsageTags.LONG_TERM_STORAGE],
     },
 }
 #
