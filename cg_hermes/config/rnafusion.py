546e6af2243ab9d860842b1b6bd3b6612a23dc09
Vadym <vadym.ivanchuk@scilifelab.se>
2024-08-06 15:17:52 +0200

    Add usage tags rnafusion

--- cg_hermes/config/rnafusion.py
+++ cg_hermes/config/rnafusion.py
@@ -5,69 +5,70 @@ mandatory by default. However the tags that are available to a particular analys
 """
 
 from cg_hermes.config.nextflow import NEXTFLOW_TAGS
+from cg_hermes.constants.tags import UsageTags
 
 RNAFUSION_COMMON_TAGS = {
     frozenset({"arriba"}): {
         "is_mandatory": True,
         "bundle_id": True,
         "tags": ["arriba", "fusion"],
-        "used_by": ["deliver"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"arriba-visualisation", "arriba"}): {
         "is_mandatory": False,
         "bundle_id": True,
         "tags": ["arriba-visualisation", "visualization", "arriba", "research"],
-        "used_by": ["deliver", "scout"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"fusioncatcher"}): {
         "is_mandatory": True,
         "tags": ["fusioncatcher", "fusion"],
-        "used_by": ["deliver"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"fusioncatcher-summary", "fusioncatcher"}): {
         "is_mandatory": True,
         "tags": ["fusioncatcher-summary"],
-        "used_by": ["deliver"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"star-fusion"}): {
         "is_mandatory": True,
         "tags": ["star-fusion", "fusion"],
-        "used_by": ["deliver"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"fusionreport", "report"}): {
         "is_mandatory": True,
         "tags": ["fusionreport", "research"],
-        "used_by": ["deliver", "scout"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"fusioninspector", "report"}): {
         "is_mandatory": False,
         "tags": ["fusioninspector"],
-        "used_by": ["deliver"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"fusioninspector-html", "report"}): {
         "is_mandatory": False,
         "tags": ["fusioninspector-html", "research"],
-        "used_by": ["deliver", "scout"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"multiqc-html", "report"}): {
         "is_mandatory": True,
         "tags": ["multiqc-html", "rna"],
-        "used_by": ["deliver", "scout"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"star-fusion-cram", "star-fusion"}): {
         "is_mandatory": True,
         "tags": ["cram"],
-        "used_by": ["deliver", "scout"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"star-fusion-cram-index", "star-fusion"}): {
         "is_mandatory": True,
         "tags": ["cram-index"],
-        "used_by": ["deliver", "scout"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"star-align-gene-counts", "star-align"}): {
         "is_mandatory": True,
         "tags": ["gene-counts"],
-        "used_by": ["deliver"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"multiqc-json"}): {
         "is_mandatory": True,
@@ -77,7 +78,7 @@ RNAFUSION_COMMON_TAGS = {
     frozenset({"vcf-fusion", "vcf-collect"}): {
         "is_mandatory": False,
         "tags": ["vcf-fusion"],
-        "used_by": ["deliver", "scout"],
+        "used_by": [UsageTags.CLINICAL_DELIVERY],
     },
     frozenset({"multiqc-fastp", "multiqc"}): {
         "is_mandatory": True,
